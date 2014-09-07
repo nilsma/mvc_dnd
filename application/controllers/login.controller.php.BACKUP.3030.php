758bb19d6761c9238d67d52678854647851146da
Nils Martinussen <nilsma231@gmail.com>
2014-09-07 20:32:57 +0200

    git merge incident

---
+++ application/controllers/login.controller.php.BACKUP.3030.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * a controller class for the application's login page
+ */
+if(!class_exists('Login_Controller')) {
+
+    class Login_Controller extends Base_Controller {
+
+        public function __construct(Login_Model $model) {
+            $this->model = $model;
+            parent::__construct($model);
+        }
+
+        public function getLoginErrors($username, $password) {
+            $errors = array();
+
+            if(!isset($username) || empty($username)) {
+                $errors[] = 'You must enter your username';
+            }
+
+            if(!isset($password) || empty($password)) {
+                $errors[] = 'You must enter your password';
+            }
+
+            if(!$this->model->validateLogin($username, $password)) {
+                $errors[] = 'Wrong username or password';
+            }
+
+            return $errors;
+        }
+
+        public function login($username, $password) {
+            $errors = $this->getLoginErrors($username, $password);
+
+            if(count($errors) < 1) {
+                $_SESSION['auth'] = true;
+                $_SESSION['username'] = $_POST['username'];
+<<<<<<< HEAD
+                $_SESSION['user_id'] = $this->model->getUserId($username);
+=======
+                $_SESSION['user_id'] = $this->model->getUserId($_POST['username']);
+>>>>>>> 6c48419ade6997b3a7f2af5b9693a91973548a78
+                header('Location: member.php');
+            } else {
+                $_SESSION['errors'] = $errors;
+            }
+        }
+
+    }
+
+}
